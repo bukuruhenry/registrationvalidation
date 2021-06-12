772aebe17f56e5ff3bd5fdbcdaee44cb3ac6686e
Henry Bukuru <30518721+bukuruhenry@users.noreply.github.com>
2021-06-12 15:20:40 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -9,7 +9,6 @@ include('registration_process.php');
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
       <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
-      <link rel="stylesheet" type="text/css" href="assets/css/registration_style.css">
       <link rel="stylesheet" type="text/css" href="assets/css/style.css">
       <!-- //web font -->
    </head>
